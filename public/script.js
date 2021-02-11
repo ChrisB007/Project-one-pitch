8fbaffba45d575a0670f72c916adba6565117e9c
ChrisB007 <chrisbitoy@gmail.com>
2021-02-11 03:58:26 -0500

    Updates to selecting player

--- public/script.js
+++ public/script.js
@@ -1,14 +1,23 @@
 /*Selecting the DOM*/
 let secondCam = document.querySelector('#peerDiv');
 let firstCam = document.querySelector('#myvid');
-const players = {};
-let button = document.querySelector('#secondcol');
+const players = {
+    playerOne : " ",
+    playerTwo : " "
+};
+let pickingPlayer = document.querySelector("#playmod");
+let button = document.querySelector('#modalsec');
 let replace = document.querySelector("#firstformdiv");
 let replace2 = document.querySelector("#secformdiv");
 let replacement = document.querySelector("#replacediv");
 let hidePlayers1 = document.querySelector("#player1");
 let hidePlayers2 = document.querySelector("#player2");
 let form = document.querySelector("form");
+let radioOne = document.querySelector("#playerradione");
+let radioTwo = document.querySelector("#playerradiotwo");
+let modalDisplay = document.querySelector("#modalsec");
+let pOneTextBox = document.querySelector("#playeronetextbox");
+let pTwoTextBox = document.querySelector("#playertwotextbox");
 let pOneInput, pTwoInput;
 
 
@@ -16,45 +25,63 @@ let pOneInput, pTwoInput;
 
  //Handles the initial clicks
 function start(){
-    //hide the forms
+    //hides the forms
     replace.classList.add("hideme");
     replace2.classList.add("hideme");
 
+    //button opens up the modal
     button.addEventListener("click", (e)=>{
-        e.preventDefault();
-        button.classList.add("hideme");
-        replacement.textContent = "Enter a number. Player with the highest number starts the game.";
-        hidePlayers1.classList.add("hideme");
-        hidePlayers2.classList.add("hideme");
-        replace.classList.remove("hideme");
-        replace2.classList.remove("hideme");
-
+        selectPlayer();
+        // replace.classList.remove("hideme");
+        // replace2.classList.remove("hideme");
+        // replacement.textContent = "Enter a number. Player with the highest number starts the game.";
+        // // button.classList.add("hideme");
+        // // hidePlayers1.classList.add("hideme");
+        // // hidePlayers2.classList.add("hideme");
 }, false);
 }
 start();
+
+
+function selectPlayer(){
+
+    var playerSelected = document.getElementsByName("payerchoice");
+
+    pickingPlayer.addEventListener("click", ()=>{
+        for (let i = 0; i < playerSelected.length; i++){
+            if (playerSelected[i].checked && playerSelected[i].value === "Player One"){
+                pOneTextBox.textContent = `You are ${playerSelected[i].value}`;
+                replace.classList.remove("hideme");
+                // document.querySelector("#secformdiv").disabled = true;
+                modalDisplay.classList.add("none");
+                replacement.textContent = "Enter a number. Player with the highest number starts the game.";
+            } else if(playerSelected[i].checked && playerSelected[i].value === "Player Two"){
+                pTwoTextBox.textContent = `You are ${playerSelected[i].value}`;
+                replace2.classList.remove("hideme");
+                // document.querySelector("#first-input").disabled = true;
+                modalDisplay.classList.add("none");
+                replacement.textContent = "Enter a number. Player with the highest number starts the game.";
+            }
+        }
+    }, false);
+};
+
 
 
 function calc(){
     // Target input's value and get values from input on click
     let firstButton = document.getElementById("button1");
     let secondButton = document.getElementById("button2");
-    
-
 
     firstButton.addEventListener("click", (e)=>{
         let firstInput = document.getElementById("first-input").value;
         if (firstInput != parseInt(firstInput)){
             alert("Please enter a number");
-            // if (parseInt(firstInput) < 1 || parseInt(firstInput) > 10){
-            //     alert("Please enter a number between 1 and 10");
-
-            // }//check if number is > 10 || < than 1;
         } else{
             pOneInput = firstInput;
             compareInput();
-            // console.log(pOneInput);
         }
-    });
+    }, false);
 
     
 
@@ -65,9 +92,8 @@ function calc(){
         } else{
             pTwoInput = secondInput;
             compareInput();
-            //  console.log(secondInput);
         }
-    });
+    }, false);
     
 }
 calc();
@@ -79,13 +105,13 @@ function compareInput(){
     if (pOneInput && pTwoInput){
         if (typeof pOneInput !== "undefined" && typeof pTwoInput !== "undefined" ){
             if (pOneInput === pTwoInput){
-                replacement.textContent = `This is a tie - Player One entered: ${pOneInput}, Player Two entered: ${pTwoInput}`;
+                replacement.textContent = `This is a tie - [Player One entered: ${pOneInput}] [Player Two entered: ${pTwoInput}]`;
                 console.log("This is a tie");
             } else if (pOneInput > pTwoInput){
-                replacement.textContent = `Player One starts the game - Player One entered: ${pOneInput}, Player Two entered: ${pTwoInput}`;
+                replacement.textContent = `Player One starts the game - [Player One entered: ${pOneInput}] [Player Two entered: ${pTwoInput}]`;
                 console.log("Player One starts the game");
             } else if (pOneInput < pTwoInput){
-                replacement.textContent = `Player Two starts the game - Player One entered: ${pOneInput}, Player Two entered: ${pTwoInput}`;
+                replacement.textContent = `Player Two starts the game - [Player One entered: ${pOneInput}] [Player Two entered: ${pTwoInput}]`;
                 console.log("Player Two starts the game");
             }
         }
@@ -99,12 +125,10 @@ function compareInput(){
 };
 
 
-// If P1 and P2 does not !== undefined
-    // If p1 === p2 console.log tie
-    //else if p1 > p2 console.log player 1's turn
-    // else if p1 < p2 conssole.log player 2's turn
 
-
+function startButton(){
+// function for the start button
+}
 
 
 
